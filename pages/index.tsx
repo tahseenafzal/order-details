848e70e95042d30e3e1b875d33e9608eb6decbe8
Tehseen Afzal <51958490+tahseenafzal@users.noreply.github.com>
2022-10-26 15:42:32 +0500

    Update index.tsx
    Create separate components of Order and Details and update in this file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,13 @@
 import { Container, Flex, VStack, Heading, Text } from "@chakra-ui/react";
+import Details from "../component/Details";
+import Order from "../component/Order";
 
 export default function Home() {
   return (
     <Container maxW={"container.xl"} p={0}>
       <Flex h={"100vh"} py={20} direction={["column-reverse", "row"]}>
-        <VStack
-          w={"full"}
-          h={"full"}
-          p={10}
-          spacing={10}
-          alignItems={"flex-start"}
-        >
-          <VStack spacing={3} alignItems={"flex-start"}>
-            <Heading size={"2xl"}>Your details</Heading>
-            <Text>If you already have a account, click here to login.</Text>
-          </VStack>
-        </VStack>
-        <VStack
-          w={"full"}
-          h={"full"}
-          p={10}
-          spacing={10}
-          alignItems={"flex-start"}
-          bg={"gray.50"}
-        >
-          <VStack spacing={3} alignItems={"flex-start"}>
-            <Heading size={"2xl"}>Your order</Heading>
-            <Text>
-              If price is too hard on your eyes, <b>try changing the theme</b>
-            </Text>
-          </VStack>
-        </VStack>
+        <Details />
+        <Order />
       </Flex>
     </Container>
   );
