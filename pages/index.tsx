43e658c1a919d3f2eea9028e64678bb6e348352c
Tehseen Afzal <abuashja@gmail.com>
2022-10-27 02:56:05 +0500

    Update index.tsx
    Apply responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import { Container, Flex, VStack, Heading, Text } from "@chakra-ui/react";
 import Details from "../component/Details";
-import Order from "../component/Order";
+import Cart from "../component/Cart";
 
 export default function Home() {
   return (
     <Container maxW={"container.xl"} p={0}>
-      <Flex h={"100vh"} py={20} direction={["column-reverse", "row"]}>
+      <Flex
+        h={{ base: "auto", md: "100vh" }}
+        py={[0, 10, 20]}
+        direction={{ base: "column-reverse", md: "row" }}
+      >
         <Details />
-        <Order />
+        <Cart />
       </Flex>
     </Container>
   );
