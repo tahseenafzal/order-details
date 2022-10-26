ebcd395050a7803a40b95af87d9308cfd7a3d365
Tehseen Afzal <abuashja@gmail.com>
2022-10-27 02:56:14 +0500

    Create theme.ts

---
+++ theme/theme.ts
@@ -0,0 +1,73 @@
+import {
+  extendTheme,
+  theme as base,
+  withDefaultColorScheme,
+  withDefaultVariant,
+} from "@chakra-ui/react";
+
+const selectInputStyle = {
+  variants: {
+    filled: {
+      field: {
+        _focus: {
+          borderColor: "brand.500",
+        },
+      },
+    },
+  },
+  sizes: {
+    md: {
+      field: {
+        borderRadius: "none",
+      },
+    },
+  },
+};
+
+const theme = extendTheme(
+  {
+    colors: {
+      brand: {
+        50: "#f5fee5",
+        100: "#e1fbb2",
+        200: "#cdf781",
+        300: "#b8ee56",
+        400: "#a2e032",
+        500: "#8ac919",
+        600: "#71ab09",
+        700: "#578602",
+        800: "#3c5e00",
+        900: "#203300",
+      },
+    },
+    Fonts: {
+      heading: `Montserrat ${base.fonts?.heading}`,
+      body: `Inter ${base.fonts?.body}`,
+    },
+    components: {
+      Input: { ...selectInputStyle },
+      Select: { ...selectInputStyle },
+      Checkbox: {
+        baseStyle: {
+          control: {
+            borderRadius: "none",
+            _focus: {
+              ring: 2,
+              ringColor: "brand.500",
+            },
+          },
+        },
+      },
+    },
+  },
+  withDefaultColorScheme({
+    colorScheme: "brand",
+    components: ["Checkbox", "Button"],
+  }),
+  withDefaultVariant({
+    variant: "filled",
+    components: ["Input", "Select"],
+  })
+);
+
+export default theme;
