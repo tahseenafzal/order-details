71f67a03ef11cf713df4ccf5463cd801f75bbda1
Tehseen Afzal <51958490+tahseenafzal@users.noreply.github.com>
2022-10-27 17:46:35 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
-import "../styles/styles.css";
+import "../styles/style.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
